30508de0928c416012efe4ed4aa1c6f60d71b8a7
Weaver23PH <themichalcarlo@gmail.com>
2019-05-04 18:42:51 +0200

    build board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,24 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from "./Board.scss";
 import Token from "../Token/Token";
-import { GameState, Player, playRound, remainderStrategy, startGame } from '@luchsamapparat/nim';
-
-const gameState: GameState = startGame({
-    heapSize: 13,
-    minTokensToRemove: 1,
-    maxTokensToRemove: 3,
-    startingPlayer: Player.Human,
-    strategy: remainderStrategy()
-});
 
 class Board extends React.Component {
     constructor(props) {
         super();
         this.state = {
-            tokens_1: [null, null, null, <Token key={this.nextId}/>, null, null, null],
-            tokens_2: [null, null, <Token/>, <Token/>, <Token/>, null, null],
-            tokens_3: [null, <Token/>, <Token/>, <Token/>, <Token/>, <Token/>, null],
-            tokens_4: [<Token/>, <Token/>, <Token/>, <Token/>, <Token/>, <Token/>, <Token/>]
+            rowNum: 0,
+            board: [],
+            tokens_1: [null, null, null, <Token key={this.nextId} />, null, null, null],
+            tokens_2: [null, null, <Token />, <Token />, <Token />, null, null],
+            tokens_3: [null, <Token />, <Token />, <Token />, <Token />, <Token />, null],
+            tokens_4: [<Token />, <Token />, <Token />, <Token />, <Token />, <Token />, <Token />]
         }
     }
     nextId() {
@@ -27,13 +20,35 @@ class Board extends React.Component {
         return this.uniqueId++;
     }
 
+    handleChange = (event) => {
+        this.setState({
+            rowNum: event.target.value
+        })
+    };
+
+    board = (rows) => {
+        let boardArr = [];
+        let i;
+        for (i = 0; i < rows; i++) {
+            let innerArr = [];
+            for (let j = 0; j < (i * 2 + 1); j++) {
+                innerArr.push(<Token key={i+j} />);
+            }
+            boardArr.push(<div className={styles.Board_row} key={i + "_row"}>{innerArr}</div>)
+        }
+        console.log(boardArr);
+        this.state.board = boardArr;
+    }
+
     render() {
         return (
             <div className={styles.Board_main}>
-                <div className={styles.Board_row}> {this.state.tokens_1} </div>
-                <div className={styles.Board_row}> {this.state.tokens_2} </div>
-                <div className={styles.Board_row}> {this.state.tokens_3} </div>
-                <div className={styles.Board_row}> {this.state.tokens_4} </div>
+                <div><label>How many rows?
+    <input type="number" placeholder="number of rows" name="rowNum" value={this.state.rowNum} onChange={(event) => this.handleChange(event)}></input>
+                </label>
+                    <button onClick={this.board(this.state.rowNum)}>Build board</button>
+                </div>
+                {this.state.board}
             </div>
         )
     }
