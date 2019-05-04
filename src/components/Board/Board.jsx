025bbdd811e7005e1864d51eda6f3ca003783c7b
Weaver23PH <themichalcarlo@gmail.com>
2019-05-04 21:11:28 +0200

    further changes

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -16,34 +16,41 @@ class Board extends React.Component {
         return this.uniqueId++;
     }
 
+    handleTokenClick = (event) => {
+        console.log(event.target + " it's me!");
+    }
+
+
     handleChange = (event) => {
-        this.setState({
-            rowNum: event.target.value
-        },
-        function() { console.log("setState completed", this.state)}
-        );
+        let value = event.target.value;
+        this.setState(prevState => ({
+            rowNum: prevState.rowNum >= 0 ? value : 0
+        }))
     };
 
-    board= (event)=>{
+    board = (event) => {
         event.preventDefault();
         let rows = parseInt(this.state.rowNum);
-        if (rows <=5){
-        let boardArr = [];
-        for (let i = 0; i < rows; i++) {
-            let innerArr = [];
-            for (let j = 0; j < (i * 2 + 1); j++) {
-                innerArr.push(<Token key={i*j+j} />);
-            }
-            boardArr.push(<div className={styles.Board_row} key={i + "_row"}>{innerArr}</div>)
-        }
 
-        this.setState({
-            board: [...boardArr]
-        });}
+        if (rows <= 5) {
+            let boardArr = [];
+            for (let i = 0; i < rows; i++) {
+                let innerArr = [];
+                for (let j = 0; j < (i * 2 + 1); j++) {
+                    let key = (Math.random() * i + j);
+                    innerArr.push(<Token key={key} onClick={(event) => this.handleTokenClick(event)} />);
+                }
+                boardArr.push(<div className={styles.Board_row} key={i + "_row"}>{innerArr}</div>)
+            }
+
+            this.setState({
+                board: [...boardArr]
+            });
+        }
         else {
             alert("Five rows is enough!");
             this.setState({
-                rowNum : 5
+                rowNum: 5
             });
         }
     }
@@ -51,12 +58,12 @@ class Board extends React.Component {
     render() {
         return (
             <div className={styles.Board_main}>
-            {this.state.board.length==0 && <form>
-                <div className={styles.Board_menu}><label>How many rows?
+                {this.state.board.length == 0 && <form>
+                    <div className={styles.Board_menu}><label>How many rows?
     <input type="number" placeholder="number of rows" name="rowNum" value={this.state.rowNum} onChange={(event) => this.handleChange(event)}></input>
-                </label>
-                    <button type="submit" onClick={(event) => this.board(event)}>Build board</button>
-                </div>
+                    </label>
+                        <button type="submit" onClick={(event) => this.board(event)}>Build board</button>
+                    </div>
                 </form>}
                 {this.state.board}
             </div>
